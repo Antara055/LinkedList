cdd6098025ce43f44c214e0a377e50da8502a829
Antara055 <antara55.dgp10.1434@gmail.com>
2021-11-08 21:37:11 +0530

    Added: add numbers into node

--- src/com/bridgelab/LinkedlistOparation.java
+++ src/com/bridgelab/LinkedlistOparation.java
@@ -3,9 +3,9 @@ package com.bridgelab;
 public class LinkedlistOparation {
     public static void addData(){
         LinkedList list = new LinkedList();
-        list.addToBegin(56);
-        list.addToBegin(30);
         list.addToBegin(70);
+        list.addToBegin(30);
+        list.addToBegin(56);
         list.displayList();
     }
 
